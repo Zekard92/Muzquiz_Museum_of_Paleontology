56f66c3ee875c4b0948401180cbc98054e7b39d9
Miguel Guajardo <mguajardoal@gmail.com>
2023-10-17 17:13:20 -0600

    Comments
    The classes had their descriptive comments missing

--- app/Application.php
+++ app/Application.php
@@ -3,13 +3,24 @@ namespace App;
 
 use App\Logger\Logger;
 
+/**
+ * The main application.
+ * 
+ * This class is in charge of the app interactions.
+ *  
+ * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+ * @copyright 2023 Zekard Technologies.
+ * @license GNU GPLv3
+ * @version 0.0.1
+ * @since 17/Oct/2023 Class available since Release 0.0.1
+ */
 class Application
 {
     public function __construct()
     {
         Logger::Log("Initializing Application");
 
-        
+        //TODO: Do things.
 
         Logger::Log("Application Initializer Concluded");
     }
--- app/Logger/Logger.php
+++ app/Logger/Logger.php
@@ -6,6 +6,11 @@ namespace App\Logger;
  * 
  * This class shall be used to write the log files for every interaction.
  * 
+ * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+ * @copyright 2023 Zekard Techonologies
+ * @license GNU GPLv3
+ * @version 0.0.1
+ * @since 17/Oct/2023 Class available since realease 0.0.1
  */
 class Logger
 {
