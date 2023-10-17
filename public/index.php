9ea494ac1da38b0e481e6a6bb72cd07752431724
Miguel Guajardo <mguajardoal@gmail.com>
2023-10-17 17:13:20 -0600

    Comments
    The classes had their descriptive comments missing

--- public/index.php
+++ public/index.php
@@ -1,23 +1,20 @@
 <?php
-/*TASK Museo.Index*/
-
-//															//Author: 	Miguel Guajardo<mguajardoal@gmail.com>
-//															//Date:		October 14th, 2023.
-
-//======================================================================================================================
-//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Every request will come through this script,
+ * This script will host and set the application up.
+ * 
+ * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+ * @copyright 2023 Zekard Technologies.
+ * @license GNU GPLv3
+ * @version 0.0.1
+ * @since 17/Oct/2023 Class available since Release 0.0.1
+ */
 
 require __DIR__ . '/../vendor/autoload.php';
 use App\Logger\Logger;
 
 Logger::Log("Request received.");
 
-// Instantiate Application;
 $app = new App\Application();
 
 Logger::Log("Request life-cycle finished.");
-
-//----------------------------------------------------------------------------------------------------------------------
-//======================================================================================================================
-/*END-TASK*/
-
