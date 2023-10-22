9562f79f650cee19459b6b29cc86e1c6f1864166
Miguel Guajardo <mguajardoal@gmail.com>
2023-10-22 07:29:03 -0600

    Logger - use log files
    Instead of echoing every log activity back to the client
    all the messages are to be sent to a log file inside app/Logger/logs/

--- app/Logger/Logger.php
+++ app/Logger/Logger.php
@@ -9,11 +9,90 @@ namespace App\Logger;
  * @author Miguel A. Guajardo <mguajardoal@gmail.com>
  * @copyright 2023 Zekard Techonologies
  * @license GNU GPLv3
- * @version 0.0.1
+ * @version 0.0.2
  * @since 17/Oct/2023 Class available since realease 0.0.1
  */
 class Logger
 {
+    private static $logFile;
+    private static $fileName;
+    private static $logsFolder;
+
+    /**
+     * Sets up logger environment.
+     * 
+     * instantiates the logs folder uri.
+     * if the folder doesn't exits, makes one.
+     * 
+     * instantiates the file uri.
+     * 
+     * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+     */
+    public static function SetEnvUp() : void
+    {
+        self::$logsFolder = __DIR__ . '/logs';
+        
+        if (!file_exists(self::$logsFolder))
+        {
+            mkdir(self::$logsFolder);
+        }
+
+        self::$fileName = self::$logsFolder . '/current.log';
+    }
+
+    /**
+     * Opens the log file.
+     * 
+     * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+     */
+    public static function OpenFile()
+    {
+        self::SetEnvUp();
+        $exists = file_exists(self::$fileName);
+        self::$logFile = fopen(self::$fileName, 'a');
+
+        if(!$exists)
+        {
+            self::Log("First Logger Entry.");
+            self::EndDivision();
+        }
+    }
+
+    /**
+     * Closes the log file
+     * 
+     * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+     */
+    public static function CloseFile()
+    {
+        fclose(self::$logFile);
+    }
+
+    /**
+     * Stores away the current log file.
+     * 
+     * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+     */
+    public static function NewLog()
+    {
+        self::CloseFile();
+        
+        $newFileName = self::$logsFolder . date('M_d_Y') . '.log';
+        rename(self::$fileName, $newFileName);
+    }
+
+    /**
+     * Writes a division to highlight a limit in the log.
+     * 
+     * @author Miguel A. Guajardo <mguajardoal@gmail.com>
+     */
+    public static function EndDivision()
+    {
+        $separationLine =
+        "------------------------------------------------------------------------------------------------------------------------\n";
+        fwrite(self::$logFile, $separationLine);
+    }
+
     /**
      * Save log message to file
      * 
@@ -29,9 +108,9 @@ class Logger
     {
         $date = date('d/M/Y H:i:s');
 
-        $message = "[$date] $level->value:\t$message";
+        $logMessage = "[$date] $level->value:\t$message";
 
-        echo "$message<br>";
+        fwrite(self::$logFile, "$logMessage\n");
     }
 
     /**
--- public/index.php
+++ public/index.php
@@ -11,19 +11,5 @@
  */
 
 require __DIR__ . '/../vendor/autoload.php';
-use App\Logger\Logger;
-
-/* */
-Logger::LogDebug("SCRIPT_URL => " . $_SERVER['SCRIPT_URL']);
-Logger::LogDebug("SCRIPT_URI => " . $_SERVER['SCRIPT_URI']);
-Logger::LogDebug("QUERY_STRING => " . $_SERVER['QUERY_STRING']);
-Logger::LogDebug("SERVER_NAME => " . $_SERVER['SERVER_NAME']);
-Logger::LogDebug("HTTP_HOST => " . $_SERVER['HTTP_HOST']);
-Logger::LogDebug("REQUEST_URI => " . $_SERVER['REQUEST_URI']);
-/* */
-
-Logger::Log("Request received.");
 
 $app = new App\Application();
-
-Logger::Log("Request life-cycle finished.");
